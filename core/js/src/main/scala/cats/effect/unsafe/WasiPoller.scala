b88b5b8f1252f2aa25fba01bdc38e2d5d7954f67
Mariusz Jakoniuk <jarmuszz@fixpoint.dev>
2026-06-11 14:15:06 +0200

    Fix WasiPoller timeout pollables outliving a poll

--- core/js/src/main/scala/cats/effect/unsafe/WasiPoller.scala
+++ core/js/src/main/scala/cats/effect/unsafe/WasiPoller.scala
@@ -4,29 +4,37 @@ import scala.scalajs.wasi
 import scala.scalajs.wit
 import scala.collection.mutable
 
-final class WasiPoller(
-    events: mutable.Queue[wasi.io.poll.Pollable]
-) {
+final class WasiPoller(events: mutable.Queue[wasi.io.poll.Pollable]) {
   val callbacks = mutable.Queue.empty[Unit => Unit]
-  var readyEvents: Array[Int] = _
+  var readyEvents: Array[Int] = null
 
   def poll(timeout: Long): PollResult =
     if (events.isEmpty) {
+      // no events means we don't have anything to poll for
       PollResult.Complete
     } else {
-      if (timeout != -1) {
+      if (timeout == -1) {
+        // Wait indefinitely for ready events
+        readyEvents = wasi.io.poll.poll(events.toArray)
+      } else {
         // adding a clock works like a timeout
         val alarm = wasi.clocks.monotonic_clock.subscribeDuration(timeout)
         events += alarm
+
+        val alarmIdx = events.length - 1
+
+        val processed = wasi.io.poll.poll(events.toArray)
+
+        /* We have to remove the alarm from events because it shouldn't outlive
+         * this poll and fire off later. We also drop its index from `readyEvents`
+         * if it got polled.
+         */
+        events.removeFirst(_ == alarm)
+        readyEvents = processed.filter(_ == alarmIdx) // this copies
       }
 
-      // Block until an I/O completes
-      readyEvents = wasi.io.poll.poll(events.toArray)
-
-      if (events.length > 0)
-        PollResult.Incomplete
-      else
-        PollResult.Complete
+      if (readyEvents == events) PollResult.Complete
+      else PollResult.Incomplete
     }
 
   def processReadyEvents(): Boolean =
@@ -34,7 +42,7 @@ final class WasiPoller(
       var did = false
       readyEvents.foreach { idx =>
         val cb = callbacks.remove(idx)
-        events.remove(idx)
+        val event = events.remove(idx)
         did = true
         cb()
       }
